2c02ee3e903c134e2aa9c208e6f5e38ed5af377d
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2025-01-08 14:34:29 +0100

    FINISH chart

--- front/components/cardChart.vue
+++ front/components/cardChart.vue
@@ -4,26 +4,36 @@
 
 <template>
     <h2 class="text-2xl font-semibold">Vos ventes</h2>
-    <div class="rounded-xl bg-white px-8 py-6">
+    <div class="rounded-xl bg-white sm:px-8 sm:py-6 px-4 py-2 ">
         <div class="flex items-center justify-between">
-            <div class="flex items-center gap-2">
-                <ButtonArrow
-                    :isAllPeriod="sales.period.value === 'all'"
-                    :isDisabled="sales.isAtMinDate.value"
-                    direction="left"
-                    :onClick="sales.navigateBack"
-                />
-                <ChartTitle />
-                <ButtonArrow
-                    :isAllPeriod="sales.period.value === 'all'"
-                    :isDisabled="sales.isAtMaxDate.value"
-                    direction="right"
-                    :onClick="sales.navigateForward"
-                />
-                <p class="text-danger" v-if="sales.clickDuringCooldown.value">Attendez avant de cliquer</p>
-            </div>
-            <ChartOption />
+    <div class="flex items-center gap-2">
+        <ButtonArrow
+            :isAllPeriod="sales.period.value === 'all'"
+            :isDisabled="sales.isAtMinDate.value"
+            direction="left"
+            :onClick="sales.navigateBack"
+        />
+        <ChartTitle />
+        <ButtonArrow
+            :isAllPeriod="sales.period.value === 'all'"
+            :isDisabled="sales.isAtMaxDate.value"
+            direction="right"
+            :onClick="sales.navigateForward"
+        />
+    </div>
+    <ChartOption />
+
+    <!-- Popup -->
+    <div 
+        v-if="sales.clickDuringCooldown.value" 
+        class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-50"
+    >
+        <div class="bg-white text-danger px-6 py-4 rounded shadow-lg text-center">
+            <p class="text-lg font-semibold">Attendez avant de cliquer</p>
         </div>
+    </div>
+</div>
+
         
         <Chart /> 
     </div>
