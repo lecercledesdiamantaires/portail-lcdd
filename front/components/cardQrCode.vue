f98527afe43062a2037507a0d56f85634c384ab0
Anthony <anthonylopes037@gmail.com>
2025-01-09 16:49:37 +0100

    UPDATE card qr code

---
+++ front/components/cardQrCode.vue
@@ -0,0 +1,47 @@
+<script setup>
+    const props  = defineProps({
+        promoCode: {
+            type: String,
+            required: true
+        }
+    })
+
+    console.log(props.promoCode)
+
+</script>
+
+
+<template>
+
+    <div class="flex flex-col gap-4 sm:w-96 xs:w-full">
+        <div>
+            <h2 class="text-2xl font-semibold">Ma carte</h2>
+        </div>
+        <div class="container-card flex flex-col rounded-3xl ">
+            <div class=" flex p-6 gap-4">
+                <img src="" alt="qr code" class="qr-code rounded-3xl h-40 w-40"/>
+                <div class="flex flex-col items-end justify-between h-full flex-1">
+                    <Logo color="white" height="h-full" width="w-full" class="h-16 w-16"/>
+                    <div class="flex flex-col items-start w-full">
+                        <h4 class="md:text-sm uppercase font-extralight text-gray-200 tracking-wider xs:text-xs" >Propriétaire</h4>
+                        <p class="md:text-lg text-white xs:text-md">Pierre Dupont</p>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom-card w-full h-full px-6 py-4 rounded-b-3xl">
+                <p class="text-xs text-white">Mon code promo :</p>
+                <p class="text-lg text-white">{{ props.promoCode }}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+   .container-card {
+      background: linear-gradient(107deg, #006D5C 2.61%, #003D34 101.2%);
+   }
+
+   .bottom-card {
+      background: linear-gradient(180deg, rgba(255, 255, 255, 0.15) 0%, rgba(255, 255, 255, 0.00) 100%);
+   }
+</style>
--- front/pages/index.vue
+++ front/pages/index.vue
@@ -28,29 +28,7 @@
 
          <CardInfosContainer />
          <div class="flex gap-4">
-            <div class="flex flex-col gap-4 sm:w-96 xs:w-full">
-            
-               <div>
-                  <h2 class="text-2xl font-semibold">Ma carte</h2>
-               </div>
-               <div class="container-card flex flex-col rounded-3xl ">
-                  <div class=" flex p-6 gap-4">
-                     <img src="" alt="qr code" class="qr-code rounded-3xl h-40 w-40"/>
-                     <div class="flex flex-col items-end justify-between h-full flex-1">
-                        <Logo color="white" height="h-full" width="w-full" class="h-16 w-16"/>
-                        <div class="flex flex-col items-start w-full">
-                           <h4 class="md:text-sm uppercase font-extralight text-gray-200 tracking-wider xs:text-xs" >Propriétaire</h4>
-                           <p class="md:text-md text-white xs:text-xs">Pierre Dupont</p>
-                        </div>
-                     </div>
-                  </div>
-                  <div class="bottom-card w-full h-full px-6 py-4 rounded-b-3xl">
-                     <p class="text-xs text-white">Mon code promo :</p>
-                     <p class="text-lg text-white">{{ promoCode }}</p>
-                  </div>
-               </div>
-   
-            </div>
+            <CardQrCode :promoCode=" promoCode"/>
             <!-- <div class="flex flex-col gap-4 flex-1">    
             </div> -->
          </div>
@@ -58,13 +36,3 @@
       </div>
    </div>
 </template>
-
-<style scoped>
-   .container-card {
-      background: linear-gradient(107deg, #006D5C 2.61%, #003D34 101.2%);
-   }
-
-   .bottom-card {
-      background: linear-gradient(180deg, rgba(255, 255, 255, 0.15) 0%, rgba(255, 255, 255, 0.00) 100%);
-   }
-</style>
