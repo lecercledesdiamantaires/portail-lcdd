5e3a79547cb8b67bd41dc73f52566b1beff2d371
cactusninjaa <thomas.sauvage@edu-devinci.fr>
2025-02-12 11:36:03 +0100

    EDIT varenv

--- back/index.js
+++ back/index.js
@@ -17,7 +17,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://partenaire.lecercledesdiamantaires.com',
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+}));
 app.use(bodyParser.json());
 app.use('/shopify', shopifyRoutes);
 
