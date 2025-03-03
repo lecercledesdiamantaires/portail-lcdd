63e2bb1a4e01e8716801e0a9ae885c6c1e59ab2d
Thomas Sauvage <144670432+cactusninjaa@users.noreply.github.com>
2025-03-03 09:53:46 +0100

    ADD terms & conditions page

--- front/middleware/vendor.js
+++ front/middleware/vendor.js
@@ -2,7 +2,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
     if (process.client) {
         const user = JSON.parse(localStorage.getItem('user'))
         const token = localStorage.getItem('token')
-
+        
         if (!token || user['role'] !== 'VENDEUR') {
             return navigateTo('/unauthorized')
         }
---
+++ front/pages/termeCondition.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="flex justify-center items-center h-full py-8">
+        <div class="w-full max-w-4xl p-8 bg-white rounded-3xl shadow-lg flex flex-col gap-4 h-full">
+            <ButtonPrimary class="self-start">
+                <NuxtLink to="/register">Retour</NuxtLink>
+            </ButtonPrimary>
+            <h1 class="text-2xl font-bold">
+                TERMS ET CONDITIONS DU PORTAIL PARTENAIRE
+            </h1>
+            <p>Dernière mise à jour : 03 mars 2025</p>
+
+            <p>Bienvenue sur le portail partenaire de Le Cercle des Diamantaires. En accédant et en utilisant ce portail, vous acceptez les présentes Conditions Générales d’Utilisation (CGU).</p>
+
+            <h2 class="text-xl font-semibold">1. Objet</h2>
+            <p>Le présent document a pour but de définir les conditions dans lesquelles Le Cercle des Diamantaires (ci-après "Nous", "Notre société") met à disposition des partenaires un portail dédié, leur permettant d’accéder à des outils, informations et services spécifiques.</p>
+
+            <h2 class="text-xl font-semibold">2. Accès au portail</h2>
+            <p>L'accès au portail est réservé aux partenaires agréés par Le Cercle des Diamantaires.
+            Chaque partenaire se voit attribuer un identifiant et un mot de passe strictement personnels.
+            Il est interdit de partager ses accès avec un tiers sans autorisation écrite.</p>
+            
+            <h2 class="text-xl font-semibold">3. Obligations des partenaires</h2>
+            <p>En utilisant le portail, le partenaire s'engage à :</p>
+
+            <p>
+                Respecter les présentes conditions d’utilisation.
+                Fournir des informations exactes et à jour.
+                Ne pas utiliser le portail à des fins frauduleuses ou illicites.
+                Maintenir la confidentialité de ses identifiants de connexion.
+            </p>
+
+            <h2 class="text-xl font-semibold">4. Propriété intellectuelle</h2>
+            <p>
+                Tous les contenus du portail (textes, images, documents, logiciels, etc.) sont protégés par des droits d’auteur et restent la propriété exclusive de Le Cercle des Diamantaires.
+                Toute reproduction, modification ou distribution sans autorisation est strictement interdite.
+            </p>
+
+            <h2 class="text-xl font-semibold">5. Confidentialité et protection des données</h2>
+            <p>
+                Les informations partagées sur le portail peuvent inclure des données sensibles et confidentielles.
+                Nous nous engageons à protéger les données personnelles des partenaires conformément aux réglementations en vigueur ([RGPD, Loi Informatique et Libertés, etc.]).
+            </p>
+
+            <h2 class="text-xl font-semibold">6. Responsabilité</h2>
+            <p>Nous nous efforçons d’assurer le bon fonctionnement du portail, mais nous ne garantissons ni l’absence d’erreurs, ni une disponibilité continue. Nous déclinons toute responsabilité en cas de perte de données, d’interruption du service ou d’utilisation non autorisée du portail par un tiers.</p>
+
+            <h2 class="text-xl font-semibold">7. Suspension et résiliation</h2>
+            <p>Nous nous réservons le droit de suspendre ou de résilier l’accès d’un partenaire en cas de :</p>
+            <p>
+                Violation des présentes conditions.
+                Utilisation abusive du portail.
+                Cessation de la relation commerciale entre le partenaire et Le Cercle des Diamantaires.
+            </p>
+            
+            <h2 class="text-xl font-semibold">8. Modification des conditions</h2>
+            <p>Nous nous réservons le droit de modifier les présentes conditions générales à tout moment. Les partenaires seront informés des changements via le portail ou par email.
+            </p>
+
+            <h2 class="text-xl font-semibold">9. Contact</h2>
+            <p>Pour toute question relative aux présentes conditions, vous pouvez nous contacter à <a href="mailto:contact@lecercledesdiamantaires.com">contact@lecercledesdiamantaires.com</a>.</p>
+        </div> 
+    </div>
+</template>
